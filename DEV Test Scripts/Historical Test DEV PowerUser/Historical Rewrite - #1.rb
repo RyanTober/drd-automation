524b23da7983ed40b98ea0b86c1c7584d00f9086
RyanTober <Ryan.Tober@sparcedge.com>
2015-08-06 15:35:19 -0400

    Removed not needed text

--- DEV Test Scripts/Historical Test DEV PowerUser/Historical Rewrite - #1.rb
+++ DEV Test Scripts/Historical Test DEV PowerUser/Historical Rewrite - #1.rb
@@ -1,11 +1,4 @@
-/class name			:class_name			:class
-css selector		:css	 
-id					:id	 
-link text			:link_text			:link
-name				:name	 
-partial link text	:partial_link_text	 
-tag name			:tag_name	 
-xpath				:xpath/
+
 
 #BROWSER: FIREFOX
 require "rubygems"
